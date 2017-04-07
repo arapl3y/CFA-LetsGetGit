95d9114b2352ce500dbcf2f92ff1d06b37d9d4ee
Hyun Kim <hkim@live.com.au>
2017-04-07 17:18:18 +1000

    added plan type column and removed title from lists

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170407062927) do
+ActiveRecord::Schema.define(version: 20170407071525) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,9 +18,9 @@ ActiveRecord::Schema.define(version: 20170407062927) do
   create_table "lists", force: :cascade do |t|
     t.integer  "user_id"
     t.boolean  "admin"
-    t.string   "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean  "plan_type"
     t.index ["user_id"], name: "index_lists_on_user_id", using: :btree
   end
 
