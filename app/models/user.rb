0587e90b4eded78865b100bbe7d14730b0b37725
Jess Tada <jesstada@gmail.com>
2017-04-08 18:10:35 +1000

    link teachers checklist to student dashboard

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,32 +1,12 @@
 class User < ApplicationRecord
   rolify
   after_create :assign_default_role, :create_todo
-  has_many :plans, :dependent => :destroy
+  has_many :plans, :Uddependent => :destroy
   has_many :lists, :dependent => :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:facebook]
-
- def self.from_omniauth(auth)
-   where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-     user.email = auth.info.email
-     user.password = Devise.friendly_token[0,20]
-     # user.name = auth.r info.name   # assuming the user model has a name
-     # user.image = auth.info.image # assuming the user model has an image
-     # If you are using confirmable and the provider(s) you use validate emails,
-     # uncomment the line below to skip the confirmation emails.
-     # user.skip_confirmation!
-   end
- end
-
- def self.new_with_session(params, session)
-   super.tap do |user|
-    if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
-    user.email = data["email"] if user.email.blank?
-      end
-    end
- end
+         :recoverable, :rememberable, :trackable, :validatable
 
 
 
@@ -36,8 +16,7 @@ class User < ApplicationRecord
 
   def create_todo
     id = self.id
-    @list = List.create(user_id: id, admin: false, plan_type: false)
-    @plan = Plan.create(title:'Enter todo list here', user_id: id, list_id: @list.id)
+    @list = List.new(user_id: id, admin: false, plan_type: false)
   end
 
   def create_checklist
