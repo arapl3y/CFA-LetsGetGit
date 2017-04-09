6b8fe16eeca947df2f4a48e3479733feea53ebb1
Jesstada <jesstada@gmail.com>
2017-04-09 13:21:07 +1000

    Student checklist displays teacher’s list

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,6 +6,14 @@ class PagesController < ApplicationController
     @list = current_user.lists.first
     @plan = Plan.new
     @all_plan = Plan.all
+
+    User.all.each do |user|
+      if user.has_role? :admin
+        @admin = user
+        @admin_list = @admin.lists.first.plans
+      end
+    end
+
   end
 
 end
